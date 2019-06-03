5b1f6f78b2b4d66e9430170302d738e9cbb7c2f8
王守虎 <sszu@qq.com>
2019-06-03 15:20:41 +0800

    添加yaml

---
+++ examples/gate/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/go-chassis/go-chassis"
+	"github.com/go-chassis/go-chassis/core/lager"
+	"github.com/go-chassis/go-chassis/core/server"
+
+	_ "github.com/mi4tin/chassis-gate/gate"
+	"github.com/mi4tin/chassis-gate/examples/schemas"
+)
+
+//if you use go run main.go instead of binary run, plz export CHASSIS_HOME=/{path}/{to}/rest/server/
+
+func main() {
+	chassis.RegisterSchema("gate", &schemas.RestGateFulHello{}, server.WithSchemaID("RestHelloService1111"))
+	if err := chassis.Init(); err != nil {
+		lager.Logger.Error("Init failed." + err.Error())
+		return
+	}
+	chassis.Run()
+}
