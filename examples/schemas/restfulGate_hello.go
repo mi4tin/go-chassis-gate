34a5eda5dbca05b58bdbd1b5a952a9ce6f73eace
王守虎 <sszu@qq.com>
2019-06-08 18:40:51 +0800

    网关例子修改

--- examples/schemas/restfulGate_hello.go
+++ examples/schemas/restfulGate_hello.go
@@ -1,12 +1,15 @@
 package schemas
 
 import (
-	"log"
 	"net/http"
 
-	"fmt"
 	rf "github.com/mi4tin/go-chassis-gate/gate"
 	"math/rand"
+	"github.com/go-mesh/openlogging"
+	"github.com/go-chassis/go-chassis/client/rest"
+	"github.com/go-chassis/go-chassis/core"
+	"context"
+	"github.com/go-chassis/go-chassis/pkg/util/httputil"
 )
 
 var num = rand.Intn(100)
@@ -14,56 +17,6 @@ var num = rand.Intn(100)
 //RestGateFulHello is a struct used for implementation of restfull hello program
 type RestGateFulHello struct {
 
-}
-
-//Sayhello is a method used to reply user with hello
-func (r *RestGateFulHello) Root(b *rf.Context) {
-	b.Write([]byte(fmt.Sprintf("x-forwarded-host %s", b.ReadRequest().Host)))
-}
-
-//Sayhello is a method used to reply user with hello
-func (r *RestGateFulHello) Sayhello(b *rf.Context) {
-	id := b.ReadPathParameter("userid")
-	log.Printf("get user id: " + id)
-	log.Printf("get user name: " + b.ReadRequest().Header.Get("user"))
-	b.Write([]byte(fmt.Sprintf("user %s from %d", id, num)))
-}
-
-//Sayhello is a method used to reply user with hello
-func (r *RestGateFulHello) Sayhello1(b *rf.Context) {
-	id := b.ReadPathParameter("userid")
-	log.Printf("get user id: " + id)
-	log.Printf("get user name: " + b.ReadRequest().Header.Get("user"))
-	b.Write([]byte(fmt.Sprintf("user %s from %d", id, num)))
-}
-
-//Sayhi is a method used to reply user with hello world text
-func (r *RestGateFulHello) Sayhi(b *rf.Context) {
-	result := struct {
-		Name string
-	}{}
-	err := b.ReadEntity(&result)
-	if err != nil {
-		b.Write([]byte(err.Error() + ":hello world"))
-		return
-	}
-	b.Write([]byte(result.Name + ":hello world"))
-	return
-}
-
-// SayJSON is a method used to reply user hello in json format
-func (r *RestGateFulHello) SayJSON(b *rf.Context) {
-	reslut := struct {
-		Name string
-	}{}
-	err := b.ReadEntity(&reslut)
-	if err != nil {
-		b.WriteHeaderAndJSON(http.StatusInternalServerError, reslut, "application/json")
-		return
-	}
-	reslut.Name = "hello " + reslut.Name
-	b.WriteJSON(reslut, "application/json")
-	return
 }
 
 func (r *RestGateFulHello) Circuit(b *rf.Context) {
@@ -76,24 +29,36 @@ func (r *RestGateFulHello) Circuit(b *rf.Context) {
 	return
 }
 
+func (r *RestGateFulHello) Circuit1(b *rf.Context) {
+	req, err := rest.NewRequest("GET", "http://carts/list", nil)
+	if err != nil {
+		openlogging.Error("new request failed.")
+		return
+	}
+
+
+	//req.Header.Set("Chassis", "info")
+
+	resp, err := core.NewRestInvoker().ContextDo(context.Background(), req)
+	if err != nil {
+		openlogging.Error("do request failed.")
+		return
+	}
+	respByte:=httputil.ReadBody(resp)
+	openlogging.Info("carts Server return : " + string(respByte))
+	b.Write(respByte)
+
+	return
+}
+
 //URLPatterns helps to respond for corresponding API calls
 func (r *RestGateFulHello) URLPatterns() []rf.Route {
 	return []rf.Route{
-		{Method: http.MethodGet, Path: "/", ResourceFuncName: "Root",
-			Returns: []*rf.Returns{{Code: 200}}},
-
-		{Method: http.MethodGet, Path: "/sayhello/{userid}", ResourceFuncName: "Sayhello",
-			Returns: []*rf.Returns{{Code: 200}},IsCheckIp:true},
-		{Method: http.MethodGet, Path: "/sayhello1/{userid}", ResourceFuncName: "Sayhello1",
-			Returns: []*rf.Returns{{Code: 200}}},
-
-		{Method: http.MethodPost, Path: "/sayhi", ResourceFuncName: "Sayhi",
-			Returns: []*rf.Returns{{Code: 200}}},
-
-		{Method: http.MethodPost, Path: "/sayjson", ResourceFuncName: "SayJSON",
-			Returns: []*rf.Returns{{Code: 200}}},
 
 		{Method: http.MethodGet, Path: "/circuit", ResourceFuncName: "Circuit",
+			Returns: []*rf.Returns{{Code: 200}}},
+
+		{Method: http.MethodGet, Path: "/circuit1", ResourceFuncName: "Circuit1",
 			Returns: []*rf.Returns{{Code: 200}}},
 	}
 }
