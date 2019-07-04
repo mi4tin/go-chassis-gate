c3d8e7f33974a29814b6549590841b9f3195da85
王守虎 <sszu@qq.com>
2019-07-04 16:10:35 +0800

    修改ci相关sh

--- scripts/travis/unit_test.sh
+++ scripts/travis/unit_test.sh
@@ -5,11 +5,6 @@ echo "mode: atomic" > coverage.txt
 # Make Necessary directories needed by Test (Ideally it should get created automatically but Travis is not allowing to create it using os.MkdriAll)
 # I know this is insane but nothing can be done
 
-# For transport test
-mkdir -p $GOPATH/src/github.com/go-chassis/go-chassis/core/transport/tls
-mkdir -p $GOPATH/src/github.com/go-chassis/go-chassis/examples/discovery/server/log
-mkdir -p $GOPATH/src/github.com/go-chassis/go-chassis/examples/discovery/server/conf
-
 # For auth test
 mkdir -p $GOPATH/test/auth/conf
 mkdir -p $GOPATH/test/auth/cipher
@@ -39,7 +34,7 @@ for d in $(go list ./... | grep -v vendor |  grep -v third_party | grep -v examp
     if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
         go test -v -cover -covermode atomic -coverprofile coverage.out
         if [ -f coverage.out ]; then
-            sed '1d;$d' coverage.out >> $GOPATH/src/github.com/go-chassis/go-chassis/coverage.txt
+            sed '1d;$d' coverage.out >> $GOPATH/src/github.com/mi4tin/go-chassis-gate/coverage.txt
         fi
     fi
 done
